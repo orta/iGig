11d97cae19fb4f00f361bda1f58461e7db71861e
orta <orta@wgrids.com>
2010-08-08 17:35:11 +0100

    IR Control added

---
+++ IRController.h
@@ -0,0 +1,19 @@
+//
+//  IRController.h
+//  iGig
+//
+//  Created by Ben Maslen on 08/08/2010.
+//  Copyright 2010 wgrids. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "AppleRemote.h"
+
+@interface IRController : NSObject {
+
+  AppleRemote* remoteControl;
+
+  
+}
+
+@end
